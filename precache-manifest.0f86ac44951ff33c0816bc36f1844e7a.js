4ebaa03fe574bd9c0b6941eb7de7047a3c6aa588
Dinh Cong Hieu <hieudc@vnext.com.vn>
2020-05-29 10:34:55 +0700

    build skeleton layout with create-react-app

---
+++ precache-manifest.0f86ac44951ff33c0816bc36f1844e7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23e8897d96f061eae7e549734c4c9065",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62a420c6f793fe069a53",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7da80ef653403a47dad3",
+    "url": "/static/js/2.39a1ab45.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.39a1ab45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62a420c6f793fe069a53",
+    "url": "/static/js/main.c1e1958d.chunk.js"
+  },
+  {
+    "revision": "24fb515c0a1a5a92fe6b",
+    "url": "/static/js/runtime-main.54b63f34.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
